155c4a60a9cf5489d390637474c6e37696bac715
reza <rezapourmoridi@gmail.com>
2023-07-25 14:47:57 +0330

    adding requested changes

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>custom</title>
+    <title>{{ $result['customer']->name }}</title>
 
     <link rel="stylesheet" href="{{ (asset('css/main/owl.carousel.min.css')) }}">
     <link rel="stylesheet" href="{{ (asset('css/main/owl.theme.default.min.css')) }}">
